063805bdd0c8755dbb47bc08ab3b7542685c79d2
AlejandrLara12 <alejandro.elara@live.com>
2016-07-05 13:17:32 -0500

    test wt pemo

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,23 +7,42 @@
 // 	});
 // }
 
+// var main = function() {
+//   	$('.person').click(function() {
+//     // $('.person').toggleClass('current');
+//     // $('.more-info').hide();
+//     $(this).toggleClass('current');
+//     if($(this).hasClass('current') == true){
+//     	$('.person').removeClass('current');
+//     	$('.person').children('.more-info').hide();
+//     	$(this).addClass('current');
+//     	$(this).children('.more-info').show();
+
+//     }
+//     else{
+//     	$('.person').removeClass('current');
+//     	$(this).children('.more-info').hide();
+//     }
+
+//   });
+
+
+// }// ends
 var main = function() {
-  	$('.person').click(function() {
+    $('.picture-name-wrap').click(function() {
     // $('.person').toggleClass('current');
     // $('.more-info').hide();
-    $(this).toggleClass('current');
-    if($(this).hasClass('current') == true){
-    	$('.person').removeClass('current');
-    	$('.person').children('.more-info').hide();
-    	$(this).addClass('current');
-    	$(this).children('.more-info').show();
-
+    $($(this).parent('.person')).toggleClass('current');
+    if($($(this).parent('.person')).hasClass('current') == true){
+        $('.person').removeClass('current');
+        $('.person').children('.more-info').hide();
+        $($(this).parent('.person')).addClass('current');
+        $($(this).parent('.person')).children('.more-info').show();
     }
     else{
-    	$('.person').removeClass('current');
-    	$(this).children('.more-info').hide();
+        $('.person').removeClass('current');
+        $($(this).parent('.person')).children('.more-info').hide();
     }
-
   });
 
 
