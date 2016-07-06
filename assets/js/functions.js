8fcbe9bdb25e4afc00d4ac0e618c949393dac973
AlejandrLara12 <alejandro.elara@live.com>
2016-07-05 23:44:03 -0500

    family almost done

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,25 +3,25 @@
 // 	$('.person').click(function() {
 // 	    $('.person').removeClass('current');
 // 	    $(this).addClass('current');
-// 	    $(this).children('.more-info').show();
+// 	    $(this).children('.more-info-wrap').show();
 // 	});
 // }
 
 // var main = function() {
 //   	$('.person').click(function() {
 //     // $('.person').toggleClass('current');
-//     // $('.more-info').hide();
+//     // $('.more-info-wrap').hide();
 //     $(this).toggleClass('current');
 //     if($(this).hasClass('current') == true){
 //     	$('.person').removeClass('current');
-//     	$('.person').children('.more-info').hide();
+//     	$('.person').children('.more-info-wrap').hide();
 //     	$(this).addClass('current');
-//     	$(this).children('.more-info').show();
+//     	$(this).children('.more-info-wrap').show();
 
 //     }
 //     else{
 //     	$('.person').removeClass('current');
-//     	$(this).children('.more-info').hide();
+//     	$(this).children('.more-info-wrap').hide();
 //     }
 
 //   });
@@ -31,17 +31,17 @@
 var main = function() {
     $('.picture-name-wrap').click(function() {
     // $('.person').toggleClass('current');
-    // $('.more-info').hide();
+    // $('.more-info-wrap').hide();
     $($(this).parent('.person')).toggleClass('current');
     if($($(this).parent('.person')).hasClass('current') == true){
         $('.person').removeClass('current');
-        $('.person').children('.more-info').hide();
+        $('.person').children('.more-info-wrap').hide();
         $($(this).parent('.person')).addClass('current');
-        $($(this).parent('.person')).children('.more-info').show();
+        $($(this).parent('.person')).children('.more-info-wrap').show();
     }
     else{
         $('.person').removeClass('current');
-        $($(this).parent('.person')).children('.more-info').hide();
+        $($(this).parent('.person')).children('.more-info-wrap').hide();
     }
   });
 
