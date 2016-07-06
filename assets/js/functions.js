51bbc56f6a189df7387a4dc334d1b016b7813298
AlejandrLara12 <alejandro.elara@live.com>
2016-07-06 16:37:28 -0500

    scrolling persons

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -29,23 +29,68 @@
 
 // }// ends
 var main = function() {
-    $('.picture-name-wrap').click(function() {
-    // $('.person').toggleClass('current');
-    // $('.more-info-wrap').hide();
+
+  //showing the more info secction smoothly
+$('.picture-name-wrap').click(function() {
     $($(this).parent('.person')).toggleClass('current');
+     var duration = 400;
     if($($(this).parent('.person')).hasClass('current') == true){
         $('.person').removeClass('current');
-        $('.person').children('.more-info-wrap').hide();
+        $('.person').children('.more-info-wrap').slideUp(duration);
         $($(this).parent('.person')).addClass('current');
-        $($(this).parent('.person')).children('.more-info-wrap').show();
+        $($(this).parent('.person')).children('.more-info-wrap').slideDown(duration);
     }
     else{
         $('.person').removeClass('current');
-        $($(this).parent('.person')).children('.more-info-wrap').hide();
+        $($(this).parent('.person')).children('.more-info-wrap').slideUp(duration);
+    }
+
+
+    var topOffset = ($(this).offset().top - $(window).scrollTop());
+    console.log(topOffset);
+    var target = $(this);
+    console.log(target);
+    if(topOffset >= 40){ 
+        $('html,body').animate({
+            scrollTop: (target.offset().top - 50)
+        },300);
+    }
+    else if(topOffset <= -40){
+        $('html,body').animate({
+            scrollTop: (target.offset().top + 50)
+        },300);
     }
   });
 
 
-}// ends
+
+// scrolling to the pages
+$('.son').click(function(event){
+    var target =  $( $(this).attr('href') );
+      if( target.length ){
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        },300);
+      }
+});
+// $('.son').click(smoothScroll(600));
+
+//   function smoothScroll(event,duration) {
+//     $('a[href^="#"]').on('click',function(event) {
+
+//       var target =  $( $(this).attr('href') );
+//       console.log(target);
+//       if( target.length ){
+//         event.preventDefault();
+//         $('html, body').animate({
+//           scrollTop: target.offset().top
+//         },duration);
+//       }
+//     });
+//   }
+
+
+}// ends main
 
 $(document).ready(main);
